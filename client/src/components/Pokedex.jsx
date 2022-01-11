fd0bde6960c9231841d0e94e0f6ec9192c808c49
Jacob Dye <jacobdye@pop-os.localdomain>
2022-01-10 19:31:41 -0500

    Still working on images but I'm close

--- client/src/components/Pokedex.jsx
+++ client/src/components/Pokedex.jsx
@@ -4,44 +4,40 @@ import { Link } from "react-router-dom"
 
 export default function Pokedex() {
     const [pokemon, setPokemon] = useState([])
-    const [pokemonImages, setPokemonImages] = useState([])
-    // const [id, setId] = useState()
     useEffect(() => {
+
         const fetchPokemon = async () => {
-            
-            const url = 'https://pokeapi.co/api/v2/pokedex/national/'
-            const res = await axios.get(url)
-            // console.log(res.data.pokemon_entries)
-            // console.log(res.data.pokemon_entries)
-            setPokemon(res.data.pokemon_entries)
+                const url = `https://pokeapi.co/api/v2/pokemon?limit=898/`
+                const res = await axios.get(url)
+                // console.log(res.data)
+                let allPokemon = res.data
+                // setAllPokemon(res.data)
+                allPokemon.results.forEach((pokeData) => {
+                    const fetchPokemonData = async () => {
+                    const url2 = pokeData.url
+                    const res2 = await axios.get(url2)
+                    // console.log(res2.data)
+                    setPokemon(res2.data)
+                    }
+                    fetchPokemonData()
+                })
         }
         fetchPokemon()
-
-        const fetchPokemonImages = async () => {
-            // for (let i = 1; i <= 898; i++) {
-                const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-                const res = await axios.get(url)
-                // setId(res.data.id)
-                console.log(res.data.id)
-                setPokemonImages(res.data)
-            // }
-        }
-        fetchPokemonImages()
     }, [])
 
 
     return (
         <div>
             <ul>
-                {/* {pokemonImages.map((poke) => {
-                    return ( */}
+                {pokemon.forEach((poke) => {
+                    return (
                         <li key={poke.id} id = 'list'>
-                            <h3 className="listitems">
-                                {poke.sprites?.front_default}
-                            </h3>
+                            <div>
+                            <img src={poke.sprites?.front_default} className="listitems" alt= {poke.name} />
+                                </ div>
                         </li>
-                    {/* ) */}
-                 {/* })} */}
+                    )
+                 })}
             </ul>
             
         </div>
